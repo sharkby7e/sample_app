f9b8bb4399849cefad01e88a0150fa94a0348c1a
sharkby7e <sgquin@gmail.com>
2024-06-07 11:21:27 -0400

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,21 +5,34 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
+  test 'should get root' do
+    get root_url
+
+    assert_response :success
+    assert_select 'title', "Home\n      | #{@base_title}"
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', "Home | #{@base_title}"
+    assert_select 'title', "Home\n      | #{@base_title}"
   end
 
   test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', "Help | #{@base_title}"
+    assert_select 'title', "Help\n      | #{@base_title}"
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', "About\n      | #{@base_title}"
+  end
+
+  test 'should get contact' do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select 'title', "Contact\n      | #{@base_title}"
   end
 end
