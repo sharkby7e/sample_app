5b92fbbe4465d0eb016b3ab7df545f7646f8c86b
sharkby7e <sgquin@gmail.com>
2024-07-01 11:13:49 -0400

    custom route names, integration tests, css simplification

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
 module ActiveSupport
   class TestCase
@@ -9,6 +9,7 @@ module ActiveSupport
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
+    include ApplicationHelper
 
     # Add more helper methods to be used by all tests here...
   end
