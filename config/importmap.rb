d18e6282740e6b2dede08ac59ddfd04f637b5039
sharkby7e <sgquin@gmail.com>
2024-08-25 01:33:28 -0400

    use stimulus for the dropdown instead of bootstrap's bullshit

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application'
+pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin 'stimulus-dropdown' # @2.1.0
+pin 'hotkeys-js' # @3.13.7
+pin 'stimulus-use' # @0.51.3
+
+pin_all_from 'app/javascript/controllers', under: 'controllers'
