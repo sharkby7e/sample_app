0b37a4874cf54de325f854b58837b105b921fc0c
sharkby7e <sgquin@gmail.com>
2024-08-26 00:03:54 -0400

    get jquery and bootstrap js working for the dropdown as well

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,9 @@ pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin 'stimulus-dropdown' # @2.1.0
 pin 'hotkeys-js' # @3.13.7
+pin 'jquery', to: 'https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.js'
+pin 'popper', to: 'popper.js', preload: true
+pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin 'stimulus-use' # @0.51.3
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
