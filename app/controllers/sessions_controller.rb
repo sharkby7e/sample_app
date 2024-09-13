da6c11191cb6db6576ea619e1741d3f84e7b92d9
sharkby7e <sgquin@gmail.com>
2024-09-13 11:12:47 -0400

    remember me box and tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       # something
       login user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to current_user
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,7 +23,7 @@ module SessionsHelper
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
-        log_in user
+        login user
         @current_user = user
       end
     end
